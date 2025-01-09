f3754f14840947dd07ee6fc5e4d4374e461834cd
Pakin <philgus884@gmail.com>
2025-01-09 20:28:11 +0700

    did ex1

--- src/main/java/Ex1_6580081/NewMain.java
+++ src/main/java/Ex1_6580081/NewMain.java
@@ -1,3 +1,4 @@
+//Nitchayanin Thamkunanon 6580081
 package Ex1_6580081;
 import java.util.*;
 
@@ -5,42 +6,59 @@ public class NewMain {
     public static int readHour()
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Hour Digit (0-23) ");
-        int hr = sc.nextInt();
+        int hr;
+        while(true)
+        {
+            System.out.println("Enter Hour Digit (0-23) = ");
+            hr = sc.nextInt();
+            if(hr>23 || hr<0) System.out.println("Invalid Hour");
+            else break;
+        }
         return hr;
     }
     public static int readMin()
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter minute Digit (0-59) ");
-        int min = sc.nextInt();
+        int min;
+        while(true)
+        {
+            System.out.println("Enter minute Digit (0-59) = ");
+            min = sc.nextInt();
+            if(min>59 || min<0) System.out.println("Invalid Minute");
+            else break;
+        }
         return min;
+    }
+    public static void calculateDuration(int startHr,int startMin,int endHr,int endMin)
+    {
+        int diff = ((endHr*3600)+(endMin*60))-((startHr*3600)+(startMin*60));
+        //if diff is negative, end time is less than start time (tomorrow)
+        if(diff<0) {
+            diff += 3600 * 24; //the next day
+            System.out.println("(tomorrow)");
+        }
+        else System.out.println("(today)");
+
+        System.out.println("Duration = "+diff/3600+" hours, "+(diff-((diff/3600)*3600))/60+" minutes");
     }
     public static void main(String[] args)
     {
+        int startHr,startMin,endHr,endMin;
+        String equal = "=";
         System.out.println("=== Start Time ===");
-        int hr,min;
-        while(true)
-        {
-            hr = readHour();
-            if(hr>23 || hr<0)
-            {
-                System.out.println("Invalid Hour");
-                continue;
-            }
-            else break;
-        }
-        while(true)
-        {
-            min = readMin();
-            if(min>59 || min<0)
-            {
-                System.out.println("Invalid Min");
-                continue;
-            }
-            else break;
-        }
-        //int min = readMin();
+        startHr = readHour();
+        startMin = readMin();
 
+        System.out.println("\n=== End Time ===");
+        endHr = readHour();
+        endMin = readMin();
+
+        //display time
+        System.out.printf("Start Time = %02d:%02d, ",startHr,startMin);
+        System.out.printf("End Time = %02d:%02d ",endHr,endMin);
+
+        //calculate duration between them. start - end (in seconds)
+        calculateDuration(startHr,startMin,endHr,endMin);
+        System.out.println(equal.repeat(35));
     }
 }
