958af476b6d845eb8f1d08009d094a67ab82d6e2
NitchayaninT <nitchayanin.tha@student.mahidol.edu>
2025-01-09 15:35:34 +0700

    9/1/25

---
+++ src/main/java/Ex1_6580081/NewMain.java
@@ -0,0 +1,18 @@
+package Ex1_6580081;
+
+public class NewMain {
+    public static int readHour()
+    {
+        int hr=0;
+        return hr;
+    }
+    public static int readMin()
+    {
+        int min=0;
+        return min;
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
